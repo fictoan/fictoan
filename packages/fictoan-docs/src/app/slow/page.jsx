6295b7e469139e8f41c3571c60b975ef567634d4
Sujan Sundareswaran <sujan.sundareswaran@gmail.com>
2025-03-15 18:04:34 +0530

    Remove LoadingBar component

--- packages/fictoan-docs/src/app/slow/page.jsx
+++
@@ -1,7 +0,0 @@
-// app/slow/page.tsx
-export default async function SlowPage() {
-    // Simulate slow data fetch
-    await new Promise(resolve => setTimeout(resolve, 3000));
-
-    return <div>Slow page content...</div>;
-}
